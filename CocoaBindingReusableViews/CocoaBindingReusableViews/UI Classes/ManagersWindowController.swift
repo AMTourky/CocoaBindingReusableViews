952e66e314226860f7c8846449486f15164be76d
AMTourky <abdelhameed.tourky@radius60.com>
2016-01-01 21:26:22 +0200

    rename models folder create window controller and reference it's window to the main window

---
+++ CocoaBindingReusableViews/CocoaBindingReusableViews/UI Classes/ManagersWindowController.swift
@@ -0,0 +1,15 @@
+//
+//  ManagersWindowController.swift
+//  CocoaBindingReusableViews
+//
+//  Created by AMTourky on 1/1/16.
+//  Copyright © 2016 AMTourky. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class ManagersWindowController: NSWindowController
+{
+    
+}
